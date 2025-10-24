cfd459e5dca08a656a5c88f4233171f3b22bf89d
GlobalTritorc <159875041+GlobalTritorc@users.noreply.github.com>
2025-10-24 15:19:59 +0400

    Add files via upload

---
+++ custom_crm_integration/app.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='custom_crm_integration',
+    version='0.0.1',
+    description='Custom multi-company integration for ERPNext and CRM',
+    packages=['custom_crm_integration']
+)
---
+++ custom_crm_integration/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='custom_crm_integration',
+    version='0.0.1',
+    description='Custom multi-company integration for ERPNext and CRM',
+    packages=find_packages(),
+    zip_safe=False
+)
