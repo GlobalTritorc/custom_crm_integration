9dd985159c340e747a8d14b4c1bea13668ec97d6
Nitin Gaikwad <nitin@tritorc.com>
2025-10-27 12:02:38 +0400

    fix: Adding final missing hooks.py file

---
+++ custom_crm_integration/hooks.py
@@ -0,0 +1,6 @@
+app_name = "custom_crm_integration"
+app_title = "Custom CRM Integration"
+app_publisher = "Global Tritorc"
+app_description = "Custom logic for multi-company ERPNext integration."
+app_version = "0.0.1"
+app_license = "MIT"
